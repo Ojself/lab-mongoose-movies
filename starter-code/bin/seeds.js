bbe66e5e47157832452b4742fd0523b2ccf85194
Tormod Flesjø <tormod.flesjo@gmail.com>
2019-04-16 18:28:37 +0200

    instructor

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,29 +1,137 @@
-const mongoose = require('mongoose');
-const Movies = require('../models/movies');
+const mongoose = require("mongoose");
+const Movies = require("../models/movies");
+const Celebrities = require("../models/celebrity");
 
-const dbName = 'mongoose-movies';
+const dbName = "mongoose-movies";
 mongoose.connect(`mongodb://localhost/${dbName}`);
+
+Movies.collection.drop();
+Celebrities.collection.drop();
 
 const movies = [
   {
-    title: 'El ataque de las ara単as',
-    genre: 'Horror',
-    plot: 'Ara単as gigantes comen gente'
+    title: "Ironhack, the bootcamp from hell",
+    genre: "Horror",
+    plot:
+      "Bootcamp goes horribly wrong when mongodb, express, node, passport and bcrypt gets introduced the same week"
   },
   {
-    title: 'El moscas de las ara単as',
-    genre: 'Horror',
-    plot: 'Moscas gigantes comen gente'
+    title: "El moscas de las ara単as",
+    genre: "Horror",
+    plot: "Moscas gigantes comen gente"
   },
   {
-    title: 'El hormigas de las ara単as',
-    genre: 'Horror',
-    plot: 'Hormigas gigantes comen gente'
-  },
-]
+    title: "El hormigas de las ara単as",
+    genre: "Horror",
+    plot: "Hormigas gigantes comen gente"
+  }
+];
 
-Movies.create(movies, (err) => {
-  if (err) { throw(err) }
-  console.log(`Created ${movies.length} celebrities`)
-  mongoose.connection.close()
+const celebrities = [
+  {
+    name: "Tasos",
+    occupation: "Web dev",
+    catchPhrase: "This is my catchphrase"
+  },
+  {
+    name: "Sylvie",
+    occupation: "Web dev",
+    catchPhrase: "This is my catchphrase"
+  },
+  {
+    name: "Rémy",
+    occupation: "Web dev",
+    catchPhrase: "This is my catchphrase"
+  },
+  {
+    name: "Alina",
+    occupation: "Web dev",
+    catchPhrase: "This is my catchphrase"
+  },
+  {
+    name: "Eddy",
+    occupation: "Web dev",
+    catchPhrase: "This is my catchphrase"
+  },
+  {
+    name: "Julia",
+    occupation: "Web dev",
+    catchPhrase: "This is my catchphrase"
+  },
+  {
+    name: "Lukas",
+    occupation: "Web dev",
+    catchPhrase: "This is my catchphrase"
+  },
+  {
+    name: "Theresa",
+    occupation: "Web dev",
+    catchPhrase: "This is my catchphrase"
+  },
+  {
+    name: "Helleke",
+    occupation: "Web dev",
+    catchPhrase: "This is my catchphrase"
+  },
+  {
+    name: "Arian",
+    occupation: "Web dev",
+    catchPhrase: "This is my catchphrase"
+  },
+  {
+    name: "Fabio",
+    occupation: "Web dev",
+    catchPhrase: "This is my catchphrase"
+  },
+  {
+    name: "Antoni",
+    occupation: "Web dev",
+    catchPhrase: "This is my catchphrase"
+  },
+  {
+    name: "André",
+    occupation: "Web dev",
+    catchPhrase: "This is my catchphrase"
+  },
+  {
+    name: "Gary",
+    occupation: "Web dev",
+    catchPhrase: "This is my catchphrase"
+  },
+  {
+    name: "Jesús",
+    occupation: "Web dev",
+    catchPhrase: "This is my catchphrase"
+  },
+  {
+    name: "Nathan",
+    occupation: "Web dev",
+    catchPhrase: "This is my catchphrase"
+  },
+  {
+    name: "Aswin",
+    occupation: "Web dev",
+    catchPhrase: "This is my catchphrase"
+  },
+  {
+    name: "Hamza",
+    occupation: "Web dev",
+    catchPhrase: "This is my catchphrase"
+  }
+];
+
+Movies.create(movies, err => {
+  if (err) {
+    throw err;
+  }
+  console.log(`Created ${movies.length} movies`);
+  mongoose.connection.close();
+});
+
+Celebrities.create(celebrities, err => {
+  if (err) {
+    throw err;
+  }
+  console.log(`Created ${celebrities.length} celebrities`);
+  mongoose.connection.close();
 });
--- starter-code/models/movies.js
+++ starter-code/models/movies.js
@@ -1,18 +1,20 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const moviesSchema = new Schema({
-  title: String,
-  genre: String,
-  plot: String,
-  stars: [ 
-    {
-      type: Schema.ObjectId, ref: 'celebrity' 
-    }
-  ]
-}, {collection:'movies'})
+const moviesSchema = new Schema(
+  {
+    title: String,
+    genre: String,
+    plot: String,
+    stars: [
+      {
+        type: Schema.ObjectId,
+        ref: "celebrity"
+      }
+    ]
+  },
+  { collection: "movies" }
+);
 
-
-
-const Movies = mongoose.model('Movies', moviesSchema)
-module.exports = Movies
+const Movies = mongoose.model("Movies", moviesSchema);
+module.exports = Movies;
